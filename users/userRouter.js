bb7220fe799f687eb859ece976e9a50ce9333c59
mariam-farrukh <50977034+mariam-farrukh@users.noreply.github.com>
2019-09-04 17:02:41 -0500

    Completed User Requests

--- users/userRouter.js
+++ users/userRouter.js
@@ -38,7 +38,14 @@ router.get('/', (req, res) => {
 });
 
 router.get('/:id', (req, res) => {
-
+    const {id} = req.params;
+    Users.getById(id)
+    .then(user => {
+        res.status(200).json(user);
+    })
+    .catch(() => {
+        res.status(500).json({errorMessage: "Could not get this user"});
+    })
 });
 
 router.get('/:id/posts', (req, res) => {
@@ -53,11 +60,35 @@ router.get('/:id/posts', (req, res) => {
 });
 
 router.delete('/:id', (req, res) => {
-
+    const{id} = req.params;
+    Users.remove(id)
+    .then(id => {
+        res.status(200).json({message: "This user has been deleted!"});
+    })
+    .catch(() => {
+        res.status(500).json({errorMessage: "Could not delete user"});
+    })
 });
 
 router.put('/:id', (req, res) => {
-
+    const {id} = req.params;
+    const update = req.body;
+    Users.getById(id)
+    .then(user => {
+        if(user) {
+            Users.update(id, update) 
+            .then (updated => {
+                Users.getById(id).then(updatedUser => {
+                    res.status(200).json(updatedUser);
+                });
+            });
+        } else {
+            res.status(400).json({errorMessage: "This user does not exist"})
+        }
+    })
+    .catch(() => {
+        res.status(400).json({errorMessage: "Could not update user"});
+    })
 });
 
 //custom middleware
