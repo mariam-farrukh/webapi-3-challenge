1df905bc316972f0f2de7de4a1f33e28d9f34dba
mariam-farrukh <50977034+mariam-farrukh@users.noreply.github.com>
2019-09-04 16:43:30 -0500

    Get user posts and create new posts by user

--- users/userRouter.js
+++ users/userRouter.js
@@ -6,26 +6,35 @@ const router = express.Router();
 router.post('/', (req, res) => {
     const newUser = req.body;
     Users.insert(newUser)
-    .then(newUser => {
-        res.status(201).json(newUser);
-    })
-    .catch(() => {
-        res.status(500).json({errorMessage: "Could not create user"});
-    })
+        .then(newUser => {
+            res.status(201).json(newUser);
+        })
+        .catch(() => {
+            res.status(500).json({errorMessage: "Could not create user"});
+        })
 });
 
 router.post('/:id/posts', (req, res) => {
-
+    const user_id = req.params.id;
+    const text = req.body.text;
+    const PostInfo = {user_id, text};
+    Posts.insert(PostInfo)
+        .then(newPost => {
+            res.status(201).json(newPost);
+        })
+        .catch(() => {
+            res.status(500).json({errorMessage: "Could not create new post"});
+        })
 });
 
 router.get('/', (req, res) => {
     Users.get()
-    .then(users => {
-        res.status(200).json(users);
-    })
-    .catch(() => {
-        res.status(500).json({errorMessage: "Could not get users"})
-    })
+        .then(users => {
+            res.status(200).json(users);
+        })
+        .catch(() => {
+            res.status(500).json({errorMessage: "Could not get users"})
+        })
 });
 
 router.get('/:id', (req, res) => {
@@ -33,7 +42,14 @@ router.get('/:id', (req, res) => {
 });
 
 router.get('/:id/posts', (req, res) => {
-
+    const{id} = req.params;
+    Users.getUserPosts(id)
+    .then(userPosts => {
+        res.status(200).json(userPosts);
+    })
+    .catch(() => {
+        res.status(500).json({errorMessage: "Could not get this user's posts"});
+    })
 });
 
 router.delete('/:id', (req, res) => {
