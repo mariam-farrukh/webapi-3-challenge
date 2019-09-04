6d6154d1ef75206777c0f05699910ab4031a9b78
mariam-farrukh <50977034+mariam-farrukh@users.noreply.github.com>
2019-09-04 16:13:51 -0500

    Created get and posts requests for users

--- users/userRouter.js
+++ users/userRouter.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const Users = require('./userDb.js');
+const Posts = require('../posts/postDb.js');
 const router = express.Router();
 
 router.post('/', (req, res) => {
     const newUser = req.body;
-
+    Users.insert(newUser)
+    .then(newUser => {
+        res.status(201).json(newUser);
+    })
+    .catch(() => {
+        res.status(500).json({errorMessage: "Could not create user"});
+    })
 });
 
 router.post('/:id/posts', (req, res) => {
@@ -12,7 +19,13 @@ router.post('/:id/posts', (req, res) => {
 });
 
 router.get('/', (req, res) => {
-
+    Users.get()
+    .then(users => {
+        res.status(200).json(users);
+    })
+    .catch(() => {
+        res.status(500).json({errorMessage: "Could not get users"})
+    })
 });
 
 router.get('/:id', (req, res) => {
